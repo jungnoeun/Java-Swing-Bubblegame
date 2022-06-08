414d1ae342434e912555520bdc464eb817fc7172
lovee9523 <noeun1102@gmail,com>
2022-06-08 23:52:44 +0900

    좌우외벽에 충돌시 플레이어 정지시키기(제약)

---
+++ bubble/src/bubble/test/ex07/BubbleFrame.java
@@ -0,0 +1,95 @@
+package bubble.test.ex07;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame{
+
+	private JLabel backgroundMap;
+	private Player player; //player객체는 계속 틀어지고 있음(while의 개념)
+	
+	public BubbleFrame() {
+		initObject();
+		initSetting();
+		initListener();
+		setVisible(true);
+	}
+	
+	private void initObject() {
+		backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+		
+		//JFrame>이미지
+		setContentPane(backgroundMap);
+		
+		//JFrame>JPanel>이미지삽입
+		//backgroundMap.setSize(1000,600);
+		//add(backgroundMap);
+		
+		player = new Player();
+		add(player); // setContentPane은 JPanel자체를 바꾸는 거지만 add는 JPanel에 그냥 덧붙이는 개념이다. 
+	}
+	
+	private void initSetting() {
+		setSize(1000, 640);
+		setLayout(null); //absolute layout(자유롭게 그림을 그릴 수 있다.)
+		setLocationRelativeTo(null); //JFrame창을 가운데 배치하기. 윈도우창 모니터 가운데 띄우기
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x버튼으로 창을 끌때 JVM도 같이 종료
+	}
+	
+	//캐릭터 좌우이동
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+			
+			//키보드 클릭 이벤트
+			@Override
+			public void keyPressed(KeyEvent e) {
+				//System.out.println(e.getKeyCode());
+				
+				switch(e.getKeyCode()) { //VK_LEFT: 37, VK_RIGHT: 39, VK_UP: 38, VK_DOWN: 40
+				case KeyEvent.VK_LEFT: //key의 left를 누르면 Player의 left메서드를 호출해서 x좌표를 변경하는 것이 목적
+					if(!player.isLeft() && !player.isLeftWallCrash()) {
+						player.left();	
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if(!player.isRight() && !player.isRightWallCrash()) {
+						player.right();	
+					}
+					break;
+				case KeyEvent.VK_UP:
+					if(!player.isUp() && !player.isDown()) { 
+						player.up();	
+					}
+					break;
+				//보글보글 게임에서 키를 눌러서 캐릭터가 아래로 움직이는 모션은 없음 -> down역할은 필요x
+				/*case KeyEvent.VK_DOWN:
+					player.down();
+					break;*/
+				}
+			}
+			
+			
+			//키보드 해제 이번트 핸들러
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch(e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.setLeft(false);
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.setRight(false);
+					break;
+				}
+			}
+			
+		});
+	}
+	
+	public static void main(String[] args) {
+		new BubbleFrame();
+	}
+}
---
+++ bubble/src/bubble/test/ex07/Player.java
@@ -0,0 +1,146 @@
+package bubble.test.ex07;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// class Player -> new 가능한 애들!! 게임에 존재할 수 있음.(추상메서드를 가질 수 없다.)
+@Getter
+@Setter //boolean들은 isLeft(),isRight()같은 함수가 만들어진다. 나머지는 get-,set-이 붙는다.
+public class Player extends JLabel implements Moveable{
+	
+	//위치상태
+	private int x;
+	private int y;
+	
+	//움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+	
+	//벽에 충돌한 상태
+	private boolean leftWallCrash;
+	private boolean rightWallCrash;
+	
+	//플레이어 속도 상태
+	private final int SPEED = 3;
+	private final int JUMPSPEED = 2; // up & down
+	
+	private ImageIcon playerR,playerL; //player의 오른편 이미지, 왼편 이미지
+	
+	public Player() {
+		initObject(); //player의 이미지 삽입
+		initSetting(); //player의 초기위치 설정
+		initBackgroundPlayerService();
+	}
+	
+	private void initObject() {
+		playerR = new ImageIcon("image/playerR.png");
+		playerL = new ImageIcon("image/playerL.png");
+	}
+	private void initSetting() {
+		//player의 초기위치 설정 
+		x = 80;
+		y = 535;
+		
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+		
+		leftWallCrash = false;
+		rightWallCrash = false;
+		
+		//Player자체가 JLable이라서 그냥 PlayerR을 넣으면 됨.
+		setIcon(playerR); //"this"생략 가능. this.setIcon(PlayerR);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
+
+	private void initBackgroundPlayerService() {
+		new Thread(new BackgroundPlayerService(this)).start();
+	}
+	
+	
+	@Override
+	public void left() {
+		System.out.println("left");
+		left = true;
+		new Thread(()-> {
+			while(left) {
+				setIcon(playerL);
+				x = x-SPEED;
+				setLocation(x, y);	
+				try {
+					Thread.sleep(10); //0.01초
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
+	}
+
+	@Override
+	public void right() {
+		System.out.println("right");
+		right = true;
+		new Thread(()-> {
+			while(right) {
+				setIcon(playerR);
+				x = x+SPEED;
+				setLocation(x, y);
+				try {
+					Thread.sleep(10); //0.01초
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
+	}
+
+	//left + up, right + up
+	@Override
+	public void up() {
+		System.out.println("up");
+		up = true;
+		new Thread(() -> {
+			for(int i=0;i<130/JUMPSPEED;i++) { //for문은 상승하는 과정이다.
+				y = y-JUMPSPEED;
+				setLocation(x, y);
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			up = false; //up은 누른다고 계속 올라가는 것이 아니다.
+			down();
+		}).start();
+	}
+
+	@Override
+	public void down() {
+		System.out.println("down");
+		down = true;
+		new Thread(()->{
+			for(int i=0;i<130/JUMPSPEED;i++) { //for문은 하강하는 과정이다.
+				y = y+JUMPSPEED;
+				setLocation(x, y);
+				try {
+					Thread.sleep(3);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			down = false; //더이상 하강하지 않으므로 false로 표시를 해준다.
+			
+		}).start();
+	}
+}
